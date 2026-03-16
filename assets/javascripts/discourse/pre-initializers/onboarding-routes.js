0c775aed9654f3505d8af0a26d88ac407c3dfc41
Pushpender Singh <52063297+pushpinder6805@users.noreply.github.com>
2026-03-16 20:28:33 +0530

    Added onboarding-route-map.js

--- assets/javascripts/discourse/pre-initializers/onboarding-routes.js
+++ assets/javascripts/discourse/pre-initializers/onboarding-routes.js
@@ -1,10 +1,5 @@
 export default {
   name: "onboarding-routes",
   before: "inject-discourse-objects",
-  initialize() {
-    const Router = require("discourse/routes/discourse").default;
-    Router.map(function () {
-      this.route("onboarding", { path: "/questionnaire" });
-    });
-  },
+  initialize() {},
 };
